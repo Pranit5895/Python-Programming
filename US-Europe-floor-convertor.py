6772945a9ac973ee6a2c22c15fb9ba64a344afb6
Pranit5895 <guptapranit9@gmail.com>
2020-05-06 22:58:14 -0700

    Code added

--- US-Europe-floor-convertor.py
+++ US-Europe-floor-convertor.py
@@ -1,1 +1,4 @@
-#US-Europe Floor Convertor
+#US-Europe Floor Converto
+euro_floor= input('Enter the floor number in euro style: ')
+us_floor= int(euro_floor)+1
+print('The US style floor number for the same is:',us_floor)r

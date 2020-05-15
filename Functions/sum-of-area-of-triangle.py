53f66e1b1b56ac7b7a36bbbc1b6d105df4e8babd
Pranit5895 <guptapranit9@gmail.com>
2020-05-15 06:34:15 -0700

    code completed hence sum displayed on screen

--- Functions/sum-of-area-of-triangle.py
+++ Functions/sum-of-area-of-triangle.py
@@ -2,4 +2,6 @@ def area_triangle(base,height):
     return base*height/2
 
 area_a =area_triangle(5,4)
-area_b =area_triangle(7,3)  
+area_b =area_triangle(7,3) 
+sum = area_a + area_b
+print("The sum of both area is: " +str(sum))

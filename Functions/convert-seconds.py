eef41ac5b1f658fc129e257030fab8485b909318
Pranit5895 <guptapranit9@gmail.com>
2020-05-15 06:53:00 -0700

    Function completed for desired output

--- Functions/convert-seconds.py
+++ Functions/convert-seconds.py
@@ -2,3 +2,4 @@ def convert_seconds(seconds):
     hours = seconds // 3600
     mintues = (seconds - hours *3600) // 60
     remaining_seconds = seconds - hours * 3600 - minutes * 60
+    return hours, minute, remaining_seconds

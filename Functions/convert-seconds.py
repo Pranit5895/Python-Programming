0760ed9a69c26d663e4e60d4a0b1be325560f8d3
Pranit5895 <guptapranit9@gmail.com>
2020-05-15 06:52:03 -0700

    Remaining seconds after hours and minutes

--- Functions/convert-seconds.py
+++ Functions/convert-seconds.py
@@ -1,3 +1,4 @@
 def convert_seconds(seconds):
     hours = seconds // 3600
-    mintues = (seconds - hours *3600) // 60 
+    mintues = (seconds - hours *3600) // 60
+    remaining_seconds = seconds - hours * 3600 - minutes * 60

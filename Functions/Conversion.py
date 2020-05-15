e6689f497892308128a46a54570ee888a76c019b
Pranit5895 <guptapranit9@gmail.com>
2020-05-15 06:40:42 -0700

    Input from user as 3 parameters

--- Functions/Conversion.py
+++ Functions/Conversion.py
@@ -1,2 +1,5 @@
 def time_conversion(hours,mintues,seconds)
-    return((3600*hours)+(60*mintues)+seconds) 
+    return((3600*hours)+(60*mintues)+seconds)
+
+amount_a = time_conversion()     //input from user 3 parameters
+amount_b = time_conversion()

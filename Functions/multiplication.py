7cfb475f6ea35652b3c2c6d2fc445d652ccc57a5
Pranit5895 <guptapranit9@gmail.com>
2020-05-16 05:27:29 -0700

    Function completed

--- Functions/multiplication.py
+++ Functions/multiplication.py
@@ -4,5 +4,16 @@ def multiplication_table(number):
         result = number * multiplier
         if result > 25:
             break
-        
+        print(str(number) + "x" + str(multiplier) + "=" + str(result))
+		# Increment the variable for the loop
+		multiplier+= 1
+
+multiplication_table(3) 
+# Should print: 3x1=3 3x2=6 3x3=9 3x4=12 3x5=15
+
+multiplication_table(5) 
+# Should print: 5x1=5 5x2=10 5x3=15 5x4=20 5x5=25
+
+multiplication_table(8)	
+# Should print: 8x1=8 8x2=16 8x3=24 
 

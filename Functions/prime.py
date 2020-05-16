8e102d3e4d5c79a38a60a052790fe43d8901d192
Pranit5895 <guptapranit9@gmail.com>
2020-05-16 05:10:43 -0700

    Comments added hence code completed

--- Functions/prime.py
+++ Functions/prime.py
@@ -1,9 +1,14 @@
 def print_prime_factors(numbers):
+     # Start with two, which is the first prime
     factor =2
+     # Keep going until the factor is larger than the number
    while factor <=  number:
+        # Check if factor is a divisor of number
        if number % factor ==0:
+            # If it is, print it and divide the original number
            print(factor)
            number = number / factor
        else :
+            # If it's not, increment the factor by one
            factor = factor+1
     return "Done"

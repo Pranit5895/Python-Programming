2a31bf8c8ff35bc69b73ca83b1e0b47636a94764
Pranit5895 <guptapranit9@gmail.com>
2020-05-17 07:25:20 -0700

    Question added

---
+++ Python-Data-Structures/Assignment-6.5.py
@@ -0,0 +1,2 @@
+# Write code using find() and string slicing (see section 6.10) to extract the number at the end of the line below. Convert the extracted value to a floating poinit number and print it out.
+

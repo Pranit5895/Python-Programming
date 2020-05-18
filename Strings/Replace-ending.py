d21317b19416a61b9fb33da7bf1876b4113cb991
Pranit5895 <guptapranit9@gmail.com>
2020-05-18 05:57:53 -0700

    Sample output also added

--- Strings/Replace-ending.py
+++ Strings/Replace-ending.py
@@ -11,4 +11,12 @@ def replace_ending(sentence, old, new):
 		return new_sentence
             # Return the original sentence if there is no match
 	return sentence
+print(replace_ending("It's raining cats and cats", "cats", "dogs")) 
+# Should display "It's raining cats and dogs"
+print(replace_ending("She sells seashells by the seashore", "seashells", "donuts")) 
+# Should display "She sells seashells by the seashore"
+print(replace_ending("The weather is nice in May", "may", "april")) 
+# Should display "The weather is nice in May"
+print(replace_ending("The weather is nice in May", "May", "April")) 
+# Should display "The weather is nice in April"
 

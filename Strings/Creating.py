3fe298528837d925466b74ad5c2da54a76a83b97
Pranit5895 <guptapranit9@gmail.com>
2020-05-18 05:00:38 -0700

    Creating string with multiple lines

--- Strings/Creating.py
+++ Strings/Creating.py
@@ -18,3 +18,12 @@ print(String1)
 String1 = '''I'm a Geek and I live in a world of "Geeks"'''
 print("\nString with the use of Triple Quotes: ")
 print(String1)
+
+
+# Creating String with triple
+# Quotes allows multiple lines
+String1 = '''Geeks
+            For
+            Life'''
+print("\nCreating a multiline String: ")
+print(String1)

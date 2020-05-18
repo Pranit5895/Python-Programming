589033522e4cae5981fc636d94c7128e15970b45
Pranit5895 <guptapranit9@gmail.com>
2020-05-18 04:59:15 -0700

    Creating a string with double quotes

--- Strings/Creating.py
+++ Strings/Creating.py
@@ -5,4 +5,10 @@
 # with single Quotes 
 String1 = 'Welcome to the Geeks World'
 print("String with the use of Single Quotes: ") 
-print(String1) 
+print(String1)
+
+# Creating a String
+# with double Quotes
+String1 = "I'm a Geek"
+print("\nString with the use of Double Quotes: ")
+print(String1)

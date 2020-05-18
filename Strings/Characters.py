3ab077b451b4ea34a3f7d71d5fe6e9c19aea791f
Pranit5895 <guptapranit9@gmail.com>
2020-05-18 05:03:15 -0700

    Printing last character

--- Strings/Characters.py
+++ Strings/Characters.py
@@ -8,3 +8,7 @@ print(String1)
 # Printing First character
 print("\nFirst character of String is: ")
 print(String1[0])
+
+# Printing Last character
+print("\nLast character of String is: ")
+print(String1[-1])

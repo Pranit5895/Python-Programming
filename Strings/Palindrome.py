d4960db93ee05c7b576b8f16957f4a9b1fd62c70
Pranit5895 <guptapranit9@gmail.com>
2020-05-18 05:51:27 -0700

    Question added

--- Strings/Palindrome.py
+++ Strings/Palindrome.py
@@ -1,3 +1,6 @@
+#The is_palindrome function checks if a string is a palindrome. A palindrome is a string that can be equally read from left to right or right to left, omitting blank spaces, and ignoring capitalization. Examples of palindromes are words like kayak and radar, and phrases like "Never Odd or Even". Fill in the blanks in this function to return True if the passed string is a palindrome, False if not.
+
+
 def is_palindrome(input_string):
 	# We'll create two strings, to compare them
 	new_string = ""
@@ -15,3 +18,4 @@ def is_palindrome(input_string):
 	if new_string == reverse_string:
 		return True
 	return False
+    

8c8be0bea5c09c0d0c58c0ac1e5112676b300b13
Pranit5895 <guptapranit9@gmail.com>
2020-05-18 04:58:24 -0700

    Creating a string with single quotes

---
+++ Strings/Creating.py
@@ -0,0 +1,8 @@
+# Python Program for 
+# Creation of String 
+  
+# Creating a String  
+# with single Quotes 
+String1 = 'Welcome to the Geeks World'
+print("String with the use of Single Quotes: ") 
+print(String1) 

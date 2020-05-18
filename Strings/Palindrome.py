6a78410829ebf446ea6b8f4af69f36505ab8c826
Pranit5895 <guptapranit9@gmail.com>
2020-05-18 05:46:18 -0700

    Traversing through each letter in input string

--- Strings/Palindrome.py
+++ Strings/Palindrome.py
@@ -3,3 +3,8 @@ def is_palindrome(input_string):
 	new_string = ""
 	reverse_string = ""
 	input_string = input_string.lower()
+        # Traverse through each letter of the input string
+	for x in input_string:
+		# Add any non-blank letters to the
+		# end of one string, and to the front
+		# of the other string.

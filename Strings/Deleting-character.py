e8388e4e2cc51b5c2a51a8d61cc7861892198f0c
Pranit5895 <guptapranit9@gmail.com>
2020-05-19 04:15:23 -0700

    Deleting a character of a string

--- Strings/Deleting-character.py
+++ Strings/Deleting-character.py
@@ -3,4 +3,10 @@
   
 String1 = "Hello, I'm a Geek"
 print("Initial String: ") 
-print(String1) 
+print(String1)
+
+# Deleting a character
+# of the String
+del String1[2]
+print("\nDeleting character at 2nd Index: ")
+print(String1)

624b4d0203d3c423a6e6cea1231d5a68c5dd0217
Pranit5895 <guptapranit9@gmail.com>
2020-05-19 04:18:53 -0700

    Deleting a string

--- Strings/Deleting-string.py
+++ Strings/Deleting-string.py
@@ -4,3 +4,9 @@
 String1 = "Hello, I'm a Geek"
 print("Initial String: ") 
 print(String1)
+
+# Deleting a String
+# with the use of del
+del String1
+print("\nDeleting entire String: ")
+print(String1)

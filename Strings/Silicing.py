6acc7e7da8c10a2b1e3a1de9f564fb6a4a8cefb9
Pranit5895 <guptapranit9@gmail.com>
2020-05-19 04:03:54 -0700

    Printing characters between 3rd and 2nd last character

--- Strings/Silicing.py
+++ Strings/Silicing.py
@@ -9,3 +9,9 @@ print(String1)
 # Printing 3rd to 12th character
 print("\nSlicing characters from 3-12: ")
 print(String1[3:12])
+
+# Printing characters between
+# 3rd and 2nd last character
+print("\nSlicing characters between " +
+    "3rd and 2nd last character: ")
+print(String1[3:-2])

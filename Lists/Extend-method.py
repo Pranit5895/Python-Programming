c53b43db3d0e116e4d9f380e844c00f33c5e8189
Pranit5895 <guptapranit9@gmail.com>
2020-05-28 02:04:45 -0700

    Python program to demonstrate adding elements using extend() method

---
+++ Lists/Extend-method.py
@@ -0,0 +1,15 @@
+# Python program to demonstrate  
+# Addition of elements in a List 
+    
+# Creating a List 
+List = [1,2,3,4] 
+print("Initial List: ") 
+print(List) 
+  
+# Addition of multiple elements 
+# to the List at the end 
+# (using Extend Method) 
+List.extend([8, 'Geeks', 'Always']) 
+print("\nList after performing Extend Operation: ") 
+print(List) 
+

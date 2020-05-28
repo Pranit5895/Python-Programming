26061caa09daee2e4f6ada4c81907b62bc22fcb8
Pranit5895 <guptapranit9@gmail.com>
2020-05-28 01:59:33 -0700

    Adding element in the list

--- Lists/Adding-element.py
+++ Lists/Adding-element.py
@@ -4,4 +4,12 @@
 # Creating a List 
 List = [] 
 print("Initial blank List: ") 
-print(List) 
+print(List)
+
+# Addition of Elements
+# in the List
+List.append(1)
+List.append(2)
+List.append(4)
+print("\nList after Addition of Three elements: ")
+print(List)

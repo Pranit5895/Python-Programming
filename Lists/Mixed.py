532497216817aad30ba710f94e270acf1e143b7f
Pranit5895 <guptapranit9@gmail.com>
2020-05-28 01:57:28 -0700

    Creating list with mixed values

---
+++ Lists/Mixed.py
@@ -0,0 +1,6 @@
+# Creating a List with  
+# mixed type of values 
+# (Having numbers and strings) 
+List = [1, 2, 'Geeks', 4, 'For', 6, 'Geeks'] 
+print("\nList with the use of Mixed Values: ") 
+print(List) 

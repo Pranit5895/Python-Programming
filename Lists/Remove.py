7f5a1b480f54e08baf0f3e920081b4b330e1684c
Pranit5895 <guptapranit9@gmail.com>
2020-05-29 03:18:51 -0700

    Removing elements from list using remove() method

--- Lists/Remove.py
+++ Lists/Remove.py
@@ -6,3 +6,10 @@ List = [1, 2, 3, 4, 5, 6,
         7, 8, 9, 10, 11, 12] 
 print("Intial List: ") 
 print(List) 
+
+# Removing elements from List
+# using Remove() method
+List.remove(5)
+List.remove(6)
+print("\nList after Removal of two elements: ")
+print(List)

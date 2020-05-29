21136bee1f888a285f8d26f4916640071b431cc3
Pranit5895 <guptapranit9@gmail.com>
2020-05-29 03:17:15 -0700

    Accessing elements in a list using negative indexing

---
+++ Lists/Negative-Indexing.py
@@ -0,0 +1,11 @@
+List = [1, 2, 'Geeks', 4, 'For', 6, 'Geeks'] 
+  
+# accessing a element using 
+# negative indexing 
+print("Acessing element using negative indexing") 
+  
+# print the last element of list 
+print(List[-1]) 
+  
+# print the third last element of list  
+print(List[-3]) 

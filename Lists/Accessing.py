1facf535048e072e4b9aab51edd2237774bf4fea
Pranit5895 <guptapranit9@gmail.com>
2020-05-29 03:15:11 -0700

    Accessing element using index number

--- Lists/Accessing.py
+++ Lists/Accessing.py
@@ -3,4 +3,11 @@
   
 # Creating a List with 
 # the use of multiple values 
-List = ["Geeks", "For", "Geeks"] 
+List = ["Geeks", "For", "Geeks"]
+
+
+# accessing a element from the
+# list using index number
+print("Accessing a element from the list")
+print(List[0])
+print(List[2])

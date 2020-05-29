65318649b20c1723c818dd56254acaa5e67b756c
Pranit5895 <guptapranit9@gmail.com>
2020-05-29 03:43:02 -0700

    Slicing

---
+++ Lists/Negative-Indexing-Slicing.py
@@ -0,0 +1,11 @@
+# Creating a List 
+List = ['G','E','E','K','S','F', 
+        'O','R','G','E','E','K','S'] 
+print("Initial List: ") 
+print(List) 
+  
+# Print elements from beginning 
+# to a pre-defined point using Slice 
+Sliced_List = List[:-6] 
+print("\nElements sliced till 6th element from last: ") 
+print(Sliced_List) 

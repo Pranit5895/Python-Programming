de1360c09908712db63e5891bd50750b131dd925
Pranit5895 <guptapranit9@gmail.com>
2020-05-29 03:16:09 -0700

    Python program to demonstrate accessing of elements from list

--- Lists/Accessing.py
+++ Lists/Accessing.py
@@ -11,3 +11,14 @@ List = ["Geeks", "For", "Geeks"]
 print("Accessing a element from the list")
 print(List[0])
 print(List[2])
+
+# Creating a Multi-Dimensional List
+# (By Nesting a list inside a List)
+List = [['Geeks', 'For'] , ['Geeks']]
+
+# accessing a element from the
+# Multi-Dimensional List using
+# index number
+print("Acessing a element from a Multi-Dimensional list")
+print(List[0][1])
+print(List[1][0])

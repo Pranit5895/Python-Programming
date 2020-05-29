49bea7475ec021a3dee1a6fee4c55ac8dcaefc18
Pranit5895 <guptapranit9@gmail.com>
2020-05-29 03:20:41 -0700

    Removing element from a list using pop() method

---
+++ Lists/Pop.py
@@ -0,0 +1,14 @@
+List = [1,2,3,4,5] 
+  
+# Removing element from the  
+# Set using the pop() method 
+List.pop() 
+print("\nList after popping an element: ") 
+print(List) 
+  
+# Removing element at a  
+# specific location from the  
+# Set using the pop() method 
+List.pop(2) 
+print("\nList after popping a specific element: ") 
+print(List) 

734e907135e38dbeadf2401fd22e04920db60830
Pranit5895 <guptapranit9@gmail.com>
2020-05-29 03:18:02 -0700

    Creating a list

---
+++ Lists/Remove.py
@@ -0,0 +1,8 @@
+# Python program to demonstrate  
+# Removal of elements in a List 
+  
+# Creating a List 
+List = [1, 2, 3, 4, 5, 6,  
+        7, 8, 9, 10, 11, 12] 
+print("Intial List: ") 
+print(List) 

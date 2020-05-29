38c18c87c9540f865d99c06ec532a7ec6f247715
Pranit5895 <guptapranit9@gmail.com>
2020-05-29 03:33:04 -0700

    print elements of a range using slice operation

--- Lists/Slicing.py
+++ Lists/Slicing.py
@@ -2,4 +2,10 @@
 List = ['G','E','E','K','S','F', 
         'O','R','G','E','E','K','S'] 
 print("Intial List: ") 
-print(List) 
+print(List)
+
+# Print elements of a range
+# using Slice operation
+Sliced_List = List[3:8]
+print("\nSlicing elements in a range 3-8: ")
+print(Sliced_List)

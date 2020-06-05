e3bbdca30c9e30e55b3238f977badff024edacce
Pranit5895 <guptapranit9@gmail.com>
2020-06-05 01:53:42 -0700

    Hackerrank solution for Python

---
+++ Hackerrank/Python-If-Else.py
@@ -0,0 +1,22 @@
+import sys
+
+
+N = int(raw_input().strip())
+
+if N % 2 != 0:
+    print "Weird"
+import sys
+
+
+N = int(raw_input().strip())
+
+if N % 2 != 0:
+    print "Weird"
+else:
+    if N >= 2 and N <= 5:
+        print "Not Weird"
+    elif N >= 6 and N <= 20:
+        print "Weird"
+    elif N > 20:
+        print "Not Weird"
+

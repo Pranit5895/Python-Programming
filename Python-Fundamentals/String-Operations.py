f466ed1b7d18da8682f4a4a12acd418c59a81e24
Pranit5895 <guptapranit9@gmail.com>
2020-08-24 10:19:54 -0700

    String with mathematical operators + and *

---
+++ Python-Fundamentals/String-Operations.py
@@ -0,0 +1,11 @@
+#print(41+"Pranit");
+#print("Pranit"+41);
+#print(1+4+"Pranit"+6+7);
+'''Note: Concatenation not allowed for string and integers in print method'''
+
+print("Pranit"*4);
+print(4*"Pranit");
+'''String multiplication Works in both manner'''
+
+print("Pranit" + "Raj");
+#print("Pranit" * "gupta"); Cannot multiply strings

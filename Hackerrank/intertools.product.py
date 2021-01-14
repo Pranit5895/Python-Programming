2bdf73f53bc3e4fe1dae14d2521e056543d7bcee
Pranit5895 <guptapranit9@gmail.com>
2021-01-13 22:14:00 -0800

    Hackerrank solution for Python

---
+++ Hackerrank/intertools.product.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from itertools import product
+
+A = raw_input().split()
+A = list(map(int, A))
+B = raw_input().split()
+B = list(map(int, B))
+
+for i in product(A, B):
+    print i,
+

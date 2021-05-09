68d644ae7f273c74f2894b8ebfba64ca2cbdbb80
Pranit5895 <guptapranit9@gmail.com>
2021-05-09 17:27:39 +0530

    Python code for printing various Patterns

--- Patterns.py
+++ Patterns.py
@@ -8,7 +8,8 @@ def pattern1(level):
     for i in range(1, level + 1):
         print()
         for j in range(i):
-            print('*', end = '')
+            print('*', end='')
+
 
 def pattern2(level):
     '''This function prints the following pattern:
@@ -20,7 +21,8 @@ def pattern2(level):
     for i in range(level, 0, -1):
         print()
         for j in range(i):
-            print('*', end = '')
+            print('*', end='')
+
 
 def pattern3(level):
     '''This function prints the following pattern:
@@ -34,6 +36,7 @@ def pattern3(level):
         print(' ' * counter + '*' * i)
         counter -= 1
 
+
 def pattern4(level):
     '''This function prints the following pattern:
     ****
@@ -42,9 +45,10 @@ def pattern4(level):
        *
     '''
     counter = 0
-    for i in range(level, 0 ,-1):
+    for i in range(level, 0, -1):
         print(' ' * counter + '*' * i)
         counter += 1
+
 
 def pattern5(level):
     '''This function prints the following pattern:
@@ -54,17 +58,18 @@ def pattern5(level):
     '''
     # first loop for number of lines
     for i in range(level + 1):
-        #second loop for spaces
+        # second loop for spaces
         for j in range(level - i):
-            print (" ",end='')
+            print(" ", end='')
         # this loop is for printing stars
         for k in range(2 * i - 1):
             print("*", end='')
         print()
 
+
 if __name__ == '__main__':
     userInput = int(input('Enter the level: '))
-     pattern1(userInput)
+    pattern1(userInput)
     print()
     pattern2(userInput)
     print()
@@ -75,22 +80,23 @@ if __name__ == '__main__':
     pattern5(userInput)
     print()
 
+
 def pattern6(userInput):
-        '''
-        following is the another approach to solve pattern problems with reduced time complexity 
-        for 
-        *
-        **
-        ***
-        ****
-        *****
-        '''
+    '''
+    following is the another approach to solve pattern problems with reduced time complexity 
+    for 
+    *
+    **
+    ***
+    ****
+    *****
+    '''
 
-        num = int(input('Enter number for pattern'))
-        pattern = '*'
-        string = pattern * num
-        x = 0
+    num = int(input('Enter number for pattern'))
+    pattern = '*'
+    string = pattern * num
+    x = 0
 
-        for i in string:
-            x = x + 1
-            print(string[0:x])
+    for i in string:
+        x = x + 1
+        print(string[0:x])

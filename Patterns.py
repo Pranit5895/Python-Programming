07e93c12db87bf7fdfbc5a4f220c10a1d42e2fdb
Pranit5895 <guptapranit9@gmail.com>
2021-05-09 17:24:18 +0530

    Pattern 2

--- Patterns.py
+++ Patterns.py
@@ -9,3 +9,15 @@ def pattern1(level):
         print()
         for j in range(i):
             print('*', end = '')
+
+def pattern2(level):
+    '''This function prints the following pattern:
+    ****
+    ***
+    **
+    *
+    '''
+    for i in range(level, 0, -1):
+        print()
+        for j in range(i):
+            print('*', end = '')
